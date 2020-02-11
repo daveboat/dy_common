ac4bc0185067a8782d33b6e285f7f9a7b98bd464
David Yan <daveyan80@gmail.com>
2020-02-11 16:42:38 -0500

    added dataset_resample to pytorch_helpers.utils

--- dy_common/pytorch_helpers/utils.py
+++ dy_common/pytorch_helpers/utils.py
@@ -1,4 +1,56 @@
+import random
 import torch
+
+def dataset_resample(data_dict, class_list, sample_distribution):
+    """
+    Takes an input dict (data_dict), with dictionary values being the class labels, and returns a list of (key, value)
+    pairs which are randomly sampled according to sample_distribution, among the class samples. This is specifically
+    for image datasets.
+
+    Assumes data_dict is a dictionary of the following format: {'path_to_imagefile': class_string_or_int}, and assumes
+    len(sample_distibution) == len(class_list) == number of classes in data_dict, and in the same order as class_list
+
+    If class_list = None, assume that class_list = range(len(sample_distribution))
+
+    For example:
+    class_list = ['classa', 'classb', 'classc']
+    sample_distribution = (1000, 800, 800)
+
+    or:
+    class_list = [0, 1, 2, 3]
+    sample_distribution = (100, 100, 100, 100)
+    """
+
+    # if None was passed as class_list, assume the labels are just 0 to len(sample_distribution) - 1
+    if class_list == None:
+        class_list = range(len(sample_distribution))
+
+    num_classes = len(class_list)
+
+    assert len(sample_distribution) == num_classes, \
+        'Sample distribution values have different length ({}) than number of classes provided ({})'.format(len(sample_distribution), num_classes)
+
+    # separate data_dict into distinct class lists
+    class_lists = [[] for _ in range(num_classes)]
+    for key, value in data_dict.items():
+        class_lists[class_list.index(value)].append((key, value))
+
+    # resample each class into lists. Sample with replacement if we are sampling more samples than exist, otherwise
+    # don't use replacement
+    resampled_class_lists = [[] for _ in range(num_classes)]
+    for i in range(num_classes):
+        if sample_distribution[i] > len(class_lists[i]):
+            resampled_class_lists[i] = random.choices(class_lists[i], k=sample_distribution[i])
+        else:
+            resampled_class_lists[i] = random.sample(class_lists[i], k=sample_distribution[i])
+
+    # join into a single list
+    resampled_list = sum(resampled_class_lists, [])
+
+    # shuffle
+    random.shuffle(resampled_list)
+
+    return resampled_list
 
 def write_histograms(writer, model, epoch):
     """
